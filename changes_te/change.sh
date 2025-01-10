b8e299d67b14d29c030eb4d4978c38b791b562ba
lilaiyigitee <771635602@qq.com>
2025-01-10 05:59:27 +0000

    te_a_compress

---
+++ changes_te/change.sh
@@ -0,0 +1,6 @@
+cp /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/layernorm_linear.py /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/layernorm_linear.py.bak
+cp layernorm_linear.py /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/layernorm_linear.py
+cp /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/layernorm_mlp.py /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/layernorm_mlp.py.bak
+cp layernorm_mlp.py /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/layernorm_mlp.py
+cp /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/linear.py /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/linear.py.bak
+cp linear.py /opt/conda/envs/ModelLink/lib/python3.10/site-packages/transformer_engine/pytorch/module/linear.py
